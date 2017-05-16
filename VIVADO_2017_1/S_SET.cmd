fd4e263f49ba317baefb8a6e5d158f596968a18d
sakinder <sakinder@live.com>
2017-05-15 22:25:02 -0500

    VIVADO_2017_1
    VIVADO_2017_1 SCRIPTS

---
+++ VIVADO_2017_1/S_SET.cmd
@@ -0,0 +1,9 @@
+@set XILDIR=C:\Xilinx\Vivado
+@set VIVADO_VERSION=2017.1
+@set PARTNUMBER=5
+@set SWAPP=NA
+@set PROGRAM_ROOT_FOLDER_FILE=1
+@set DO_NOT_CLOSE_SHELL=0
+@set ZIP_PATH=C:/Sim/WEBSERVER/zynq_soc/zip/zip.exe
+@set ENABLE_SDSOC=1
+@set XILINXGIT_DEVICETREE=D:/xilinx_git/device-tree-xlnx
